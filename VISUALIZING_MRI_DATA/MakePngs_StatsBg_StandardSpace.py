03001d7017e3cbae6115fa962099262cbf34b4f1
Kirstie Whitaker <kw401@cam.ac.uk>
2015-08-21 18:26:51 +0100

    need to sort the list

--- VISUALIZING_MRI_DATA/MakePngs_StatsBg_StandardSpace.py
+++ VISUALIZING_MRI_DATA/MakePngs_StatsBg_StandardSpace.py
@@ -304,7 +304,7 @@ def crop_data(bg, stats):
     for i, sl in enumerate(slices_list):
         sl = sl + [ n - 5 for n in sl ]
         sl = sl + [ n + 5 for n in sl ]
-        slices_list[i] = list(set(sl))
+        slices_list[i] = sorted(list(set(sl)))
     
     return bg_cropped, stats_cropped, slices_list
 
